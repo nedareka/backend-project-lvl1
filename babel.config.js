004f80fd8c20bad05dffd6f5b1f485b8563d13d5
nedareka <nedareka@mail.ru>
2019-10-31 15:59:49 +0300

    Connecting Babel

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  const presets = [
+    ['@babel/preset-env',
+      {
+        targets: {
+          node: 'current'
+        },
+      },
+    ],
+  ];
+  const plugins = [];
+
+  return {
+    presets,
+    plugins
+  };
+}
