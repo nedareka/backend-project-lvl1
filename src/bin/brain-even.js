2a347583a95765df2f23e1c0cd02c9407c904d69
nedareka <nedareka@mail.ru>
2019-11-04 15:53:55 +0300

    brain-even game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,1 +1,27 @@
 #!/usr/bin/env node
+import {
+  greeting, getName, getRandInteger, ifEven, answRight,
+} from '..';
+
+greeting();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const name = getName();
+
+for (let i = 1; i <= 3; i += 1) {
+  const num = getRandInteger();
+  console.log(`Question: ${num}`);
+  if (answRight(num)) {
+    console.log('Correct!');
+    if (i === 3) {
+      console.log(`Congratulations, ${name}`);
+    }
+  } else if (ifEven(num)) {
+    console.log('"no" is wrong answer ;(. Correct answer was "yes".');
+    console.log(`Let's try again, ${name}!`);
+    break;
+  } else {
+    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+    console.log(`Let's try again, ${name}!`);
+    break;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import readlineSync from 'readline-sync';
 
+
 const greeting = () => {
   console.log('Welcome to the Brain Games');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
 };
 
-export default greeting;
+const getName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+const getRandInteger = () => {
+  const min = 0;
+  const max = 1000;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const ifEven = (n) => {
+  if (n % 2 === 0) return true;
+  return false;
+};
+
+const answRight = (num) => {
+  const ans = readlineSync.question('Your answer: ');
+  if ((ifEven(num) && (ans === 'yes')) || (!ifEven(num) && (ans === 'no'))) {
+    return true;
+  }
+  return false;
+};
+
+export {
+  greeting, getName, getRandInteger, ifEven, answRight,
+};
