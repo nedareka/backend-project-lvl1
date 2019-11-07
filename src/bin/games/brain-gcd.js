4898f924e9d0ac4f41743778330cb2b286e1ffc6
nedareka <nedareka@mail.ru>
2019-11-07 16:16:30 +0300

    Fixing a bug in brain-gcd.js

--- src/bin/games/brain-gcd.js
+++ src/bin/games/brain-gcd.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import {
-  greeting, getName, getRandInteger, getAnsw, hello, printWrong, printRight,
+  greeting, getName, getRandInt, getAnsw, hello, printWrong, printRight,
   divisor,
 } from '../..';
 
@@ -8,10 +8,12 @@ greeting();
 console.log('Find the greatest common divisor of given numbers.');
 const name = getName();
 hello(name);
+const startInt = 1;
+const endInt = 100;
 
 for (let i = 1; i <= 3; i += 1) {
-  const num1 = getRandInteger();
-  const num2 = getRandInteger();
+  const num1 = getRandInt(startInt, endInt);
+  const num2 = getRandInt(startInt, endInt);
   const gcd = divisor(num1, num2);
   console.log(`Question: ${num1} ${num2}`);
   const ans = Number(getAnsw());
