cb6fe47b7c7a489dcb99d7179f85e5f64fef0784
nedareka <nedareka@mail.ru>
2019-11-12 03:26:33 +0300

    Refact brain-even

--- src/bin/games/brain-even.js
+++ src/bin/games/brain-even.js
@@ -1,32 +1,6 @@
 #!/usr/bin/env node
 import {
-  greeting, getName, getRandInt, isEven, getAnsw, answRight, hello,
-  printWrong, printRight,
+  brainEven,
 } from '../..';
 
-greeting();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const name = getName();
-hello(name);
-const startInt = 1;
-const endInt = 100;
-const cycles = 3;
-
-for (let i = 0; i < cycles; i += 1) {
-  const num = getRandInt(startInt, endInt);
-  console.log(`Question: ${num}`);
-  const ans = getAnsw();
-  const even = isEven(num);
-
-  let key = 'no';
-
-  if (!answRight(ans, even)) {
-    if (even) {
-      key = 'yes';
-    }
-    printWrong(ans, key, name);
-    break;
-  }
-
-  printRight((i === cycles - 1), name);
-}
+brainEven();
--- src/index.js
+++ src/index.js
@@ -152,11 +152,39 @@ const brainCalc = () => {
 
     printRight((i === cycles - 1), name);
   }
-}
+};
 
+const brainEven = () => {
+  greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const name = getName();
+  hello(name);
+  const startInt = 1;
+  const endInt = 100;
+  const cycles = 3;
+
+  for (let i = 0; i < cycles; i += 1) {
+    const num = getRandInt(startInt, endInt);
+    console.log(`Question: ${num}`);
+    const ans = getAnsw();
+    const even = isEven(num);
+
+    let key = 'no';
+
+    if (!answRight(ans, even)) {
+      if (even) {
+        key = 'yes';
+      }
+      printWrong(ans, key, name);
+      break;
+    }
+
+    printRight((i === cycles - 1), name);
+  }
+};
 
 export {
   greeting, hello, getName, getRandInt, isEven, getAnsw, answRight,
   getRandSign, printWrong, printRight, divisor, makeSeq, printQuest,
-  getCalcRes, isPrime, brainCalc,
+  getCalcRes, isPrime, brainCalc, brainEven,
 };
